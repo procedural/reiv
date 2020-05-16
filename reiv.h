1c593c512b45669f6496bb455732932ca436c625
procedural <constantine@protonmail.com>
2020-05-16 04:25:48 +0300

    Port other procedures from reiii

--- reiv.h
+++ reiv.h
@@ -1,8 +1,11 @@
 #pragma once
 
+void reivScreen(unsigned x, unsigned y, unsigned w, unsigned h, const void * displayOrHinstance, const void * windowOrHwnd);
 void reivPixels(unsigned pixelsIndex, unsigned wh, const unsigned char * rgba);
 void reivMesh(unsigned meshIndex, unsigned trianglesFirst, unsigned trianglesCount, const float4 * triplet0, const float4 * triplet1, const float4 * triplet2, const float4 * triplet3);
 void reivPipeline(unsigned pipelineIndex, const void * state);
 void reivVariables(unsigned variablesCount, const float4 * variables);
 void reivDraw(unsigned meshIndex, unsigned pipelineIndex, unsigned instancesCount);
 void reivReadback(unsigned x, unsigned y, unsigned w, unsigned h, unsigned char * outRgba);
+void reivPresent(void);
+void reivDestroyEverything(void);
