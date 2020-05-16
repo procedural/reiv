6ca724a9e5f1c7f5d33f621fd84e2b14deeaeeeb
procedural <constantine@protonmail.com>
2020-05-16 04:50:58 +0300

    Cubes

--- reiv.h
+++ reiv.h
@@ -1,7 +1,7 @@
 #pragma once
 
 void reivWindow(unsigned w, unsigned h, const void * displayOrHinstance, const void * windowOrHwnd);
-void reivPixels(unsigned pixelsIndex, unsigned wh, const unsigned char * rgba);
+void reivPixels(unsigned pixelsIndex, unsigned wh, const unsigned char * rgbaFace0, const unsigned char * rgbaFace1, const unsigned char * rgbaFace2, const unsigned char * rgbaFace3, const unsigned char * rgbaFace4, const unsigned char * rgbaFace5);
 void reivMesh(unsigned meshIndex, unsigned trianglesFirst, unsigned trianglesCount, const float4 * triplets0, const float4 * triplets1, const float4 * triplets2, const float4 * triplets3);
 void reivPipeline(unsigned pipelineIndex, const void * state);
 void reivVariables(unsigned variablesCount, const float4 * variables);
