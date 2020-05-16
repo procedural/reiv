75a0f50befbefc60859686e9210ce7f29bb01f0e
procedural <constantine@protonmail.com>
2020-05-16 04:30:59 +0300

    Batch draw

--- reiv.h
+++ reiv.h
@@ -5,7 +5,7 @@ void reivPixels(unsigned pixelsIndex, unsigned wh, const unsigned char * rgba);
 void reivMesh(unsigned meshIndex, unsigned trianglesFirst, unsigned trianglesCount, const float4 * triplet0, const float4 * triplet1, const float4 * triplet2, const float4 * triplet3);
 void reivPipeline(unsigned pipelineIndex, const void * state);
 void reivVariables(unsigned variablesCount, const float4 * variables);
-void reivDraw(unsigned meshIndex, unsigned pipelineIndex, unsigned instancesCount);
+void reivDraw(unsigned count, unsigned * meshIndexes, unsigned * pipelineIndexes, unsigned * instancesCounts);
 void reivReadback(unsigned x, unsigned y, unsigned w, unsigned h, unsigned char * outRgba);
 void reivPresent(void);
 void reivDestroyEverything(void);
