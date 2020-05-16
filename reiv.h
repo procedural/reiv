13ac4e40a385a2362a66e61079b469a3f5ca5bf2
procedural <constantine@protonmail.com>
2020-05-16 05:10:22 +0300

    Allow updating pixels

--- reiv.h
+++ reiv.h
@@ -1,7 +1,7 @@
 #pragma once
 
 void reivWindow(unsigned w, unsigned h, const void * displayOrHinstance, const void * windowOrHwnd);
-void reivPixels(unsigned pixelsIndex, unsigned wh, const unsigned char * rgbaFace0, const unsigned char * rgbaFace1, const unsigned char * rgbaFace2, const unsigned char * rgbaFace3, const unsigned char * rgbaFace4, const unsigned char * rgbaFace5);
+void reivPixels(unsigned pixelsIndex, unsigned x, unsigned y, unsigned w, unsigned h, const unsigned char * rgbaFace0, const unsigned char * rgbaFace1, const unsigned char * rgbaFace2, const unsigned char * rgbaFace3, const unsigned char * rgbaFace4, const unsigned char * rgbaFace5);
 void reivMesh(unsigned meshIndex, unsigned trianglesFirst, unsigned trianglesCount, const float4 * triplets0, const float4 * triplets1, const float4 * triplets2, const float4 * triplets3);
 void reivPipeline(unsigned pipelineIndex, const void * state);
 void reivVariables(unsigned variablesIndex, unsigned values32BitCount, const void * values32Bit);
