50c3e9fdddc11eb9b8adc689fce3078bc422682a
procedural <constantine@protonmail.com>
2020-05-16 04:38:32 +0300

    These are arrays

--- reiv.h
+++ reiv.h
@@ -2,7 +2,7 @@
 
 void reivWindow(unsigned w, unsigned h, const void * displayOrHinstance, const void * windowOrHwnd);
 void reivPixels(unsigned pixelsIndex, unsigned wh, const unsigned char * rgba);
-void reivMesh(unsigned meshIndex, unsigned trianglesFirst, unsigned trianglesCount, const float4 * triplet0, const float4 * triplet1, const float4 * triplet2, const float4 * triplet3);
+void reivMesh(unsigned meshIndex, unsigned trianglesFirst, unsigned trianglesCount, const float4 * triplets0, const float4 * triplets1, const float4 * triplets2, const float4 * triplets3);
 void reivPipeline(unsigned pipelineIndex, const void * state);
 void reivVariables(unsigned variablesCount, const float4 * variables);
 void reivDraw(unsigned count, unsigned * meshIndexes, unsigned * pipelineIndexes, unsigned * instancesCounts);
