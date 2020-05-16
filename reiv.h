af35001f85e4005c3426f77be402e76256b7df68
procedural <constantine@protonmail.com>
2020-05-16 04:54:16 +0300

    Index different variables sets

--- reiv.h
+++ reiv.h
@@ -4,7 +4,7 @@ void reivWindow(unsigned w, unsigned h, const void * displayOrHinstance, const v
 void reivPixels(unsigned pixelsIndex, unsigned wh, const unsigned char * rgbaFace0, const unsigned char * rgbaFace1, const unsigned char * rgbaFace2, const unsigned char * rgbaFace3, const unsigned char * rgbaFace4, const unsigned char * rgbaFace5);
 void reivMesh(unsigned meshIndex, unsigned trianglesFirst, unsigned trianglesCount, const float4 * triplets0, const float4 * triplets1, const float4 * triplets2, const float4 * triplets3);
 void reivPipeline(unsigned pipelineIndex, const void * state);
-void reivVariables(unsigned variablesCount, const float4 * variables);
+void reivVariables(unsigned variablesIndex, unsigned variablesCount, const float4 * variables);
 void reivDraw(unsigned count, unsigned * meshIndexes, unsigned * pipelineIndexes, unsigned * instancesCounts);
 void reivReadback(unsigned x, unsigned y, unsigned w, unsigned h, unsigned char * outRgba);
 void reivPresent(void);
