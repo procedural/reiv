315aa77684af9b42af3feca2fbc695b183bb8a5e
procedural <constantine@protonmail.com>
2020-05-16 11:25:16 +0300

    Update reiv.h

--- reiv.h
+++ reiv.h
@@ -7,7 +7,7 @@ void reivMeshAos(unsigned meshIndex, unsigned trianglesFirst, unsigned triangles
 void reivMeshSoa(unsigned meshIndex, unsigned trianglesFirst, unsigned trianglesCount, const float * triplets0x, const float * triplets0y, const float * triplets0z, const float * triplets0w, const float * triplets1x, const float * triplets1y, const float * triplets1z, const float * triplets1w, const float * triplets2x, const float * triplets2y, const float * triplets2z, const float * triplets2w, const float * triplets3x, const float * triplets3y, const float * triplets3z, const float * triplets3w);
 void reivPipeline(unsigned pipelineIndex, const void * state);
 void reivVariables(unsigned variablesIndex, unsigned values32BitCount, const void * values32Bit);
-void reivDraw(unsigned count, unsigned * meshIndexes, unsigned * pipelineIndexes, unsigned * instancesCounts);
+void reivDraw(unsigned count, const unsigned * meshIndexes, const unsigned * pipelineIndexes, const unsigned * instancesCounts);
 void reivReadback(unsigned x, unsigned y, unsigned w, unsigned h, unsigned char * outRgba);
 void reivPresent(void);
 void reivDestroyEverything(void);
