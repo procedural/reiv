fee4a1b8cf4b4189ff801ab396705566b831e46e
procedural <constantine@protonmail.com>
2020-05-16 04:07:22 +0300

    Create reiv.h

---
+++ reiv.h
@@ -0,0 +1,7 @@
+#pragma once
+
+void reivPixels(unsigned pixelsIndex, unsigned wh, const unsigned char * rgba);
+void reivMesh(unsigned meshIndex, unsigned trianglesFirst, unsigned trianglesCount, const float4 * triplet0, const float4 * triplet1, const float4 * triplet2, const float4 * triplet3);
+void reivPipeline(unsigned pipelineIndex, const void * state);
+void reivVariables(unsigned variablesCount, const float4 * variables);
+void reivDraw(unsigned meshIndex, unsigned pipelineIndex, unsigned instancesCount);
